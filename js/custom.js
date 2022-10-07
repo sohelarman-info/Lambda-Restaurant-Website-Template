20038ca57db858f5255148f19e321f7883f008a0
Sohel Arman <sohelarman.info@gmail.com>
2022-10-08 00:08:06 +0600

    Lambda Resaurant Website Design

---
+++ js/custom.js
@@ -0,0 +1,61 @@
+// Wow animate slider js start (https://animate.style/)
+new WOW().init();
+// Wow animate slider js end
+
+
+// slick slider js start
+$('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+// slick slider js end
+
+
+// navbar js start
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('.navbar').addClass('fixed');
+		} else {
+			$('.navbar').removeClass('fixed');
+		}
+	});
+});
+// navbar js end
+
